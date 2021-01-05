50518152641c99d3308d28b480e136a0fb4ecfa8
“John <john@reactnative.guru>
2021-01-05 18:58:05 +0000

    field wrapper

--- src/components/Form/FieldWrapper.js
+++ src/components/Form/FieldWrapper.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import colors from '../../config/colors';
 
-const FieldWrapper = (props) => (
-  <View style={styles.container}>
-    <Text>FieldWrapper</Text>
-  </View>
-);
+const FieldWrapper = ({
+  children,
+  label = '',
+  message = '',
+  messageType = '',
+}) => {
+  const messageStyles = [styles.messageText];
+
+  if (messageType === 'success') {
+    messageStyles.push(styles.messageSuccess);
+  } else if (messageType === 'error') {
+    messageStyles.push(styles.messageError);
+  }
+  return (
+    <View style={styles.container}>
+      <Text>{label}</Text>
+      {children}
+      <Text style={messageStyles}>{message}</Text>
+    </View>
+  );
+};
 export default FieldWrapper;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  labelText: {
+    color: colors.gray,
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  messageText: {
+    color: colors.gray,
+    fontSize: 13,
+    marginBottom: 5,
+  },
+  messageSuccess: {
+    color: colors.success,
+  },
+  messageError: {
+    color: colors.error,
   },
 });
--- src/stories/Form.stories.js
+++ src/stories/Form.stories.js
@@ -3,10 +3,59 @@ import {View, Text} from 'react-native';
 import {storiesOf} from '@storybook/react-native';
 import {action} from '@storybook/addon-actions';
 
-import {Form} from '../components/Form';
+import {Form, FieldWrapper} from '../components/Form';
 import {BufferView} from './decorators';
 
 // use the storiesOf function to create the actual story
+
+storiesOf('Form/FieldWrapper', module)
+  .addDecorator(BufferView)
+  .add('default', () => (
+    <FieldWrapper label="Email">
+      <View style={{flex: 1, backgroundColor: '#e6e6e6'}}>
+        <Text>Hello wrpapper</Text>
+      </View>
+    </FieldWrapper>
+  ))
+  .add('error message', () => (
+    <FieldWrapper
+      label="Email"
+      message="Please enter your email"
+      messageType="error">
+      <View style={{flex: 1, backgroundColor: '#e6e6e6'}}>
+        <Text>Wrapper Text</Text>
+      </View>
+    </FieldWrapper>
+  ))
+  .add('success message', () => (
+    <FieldWrapper label="Email" message="Looks good" messageType="error">
+      <View style={{flex: 1, backgroundColor: '#e6e6e6'}}>
+        <Text>Success</Text>
+      </View>
+    </FieldWrapper>
+  ))
+  .add('multiple fields', () => (
+    <React.Fragment>
+      <FieldWrapper label="Email">
+        <View style={{flex: 1, backgroundColor: '#e6e6e6'}}>
+          <Text>Hello wrpapper</Text>
+        </View>
+      </FieldWrapper>
+      <FieldWrapper
+        label="Email"
+        message="Please enter your email"
+        messageType="error">
+        <View style={{flex: 1, backgroundColor: '#e6e6e6'}}>
+          <Text>Wrapper Text</Text>
+        </View>
+      </FieldWrapper>
+      <FieldWrapper label="Email" message="Looks good" messageType="error">
+        <View style={{flex: 1, backgroundColor: '#e6e6e6'}}>
+          <Text>Success</Text>
+        </View>
+      </FieldWrapper>
+    </React.Fragment>
+  ));
 
 storiesOf('Form', module)
   .addDecorator(BufferView)
